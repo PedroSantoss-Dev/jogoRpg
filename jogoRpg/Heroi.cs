d64c66b92cb0bd4b90d4330318f319380654959d
Pedro Santos <90013568+PedroSantoss-Dev@users.noreply.github.com>
2023-08-11 00:16:00 -0300

    feat: create class Heroi

---
+++ jogoRpg/Heroi.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jogoRpg
+{
+    public class Heroi
+    {
+        public String Nome { get; set; }
+        public int Vida { get; set; }
+        public int Ataque { get; set; }
+        public int Nivel { get; set; }
+        public int Xp { get; set; }
+        public int AtaqueBase { get; set; }
+
+        public Heroi(String nome)
+        {
+            Random Aleatorio = new Random();
+            this.Nome = nome;
+            this.Nivel = 1;
+            this.Vida = 10;
+            this.Ataque = AtaqueBase + Nivel;
+            this.Xp = 0;
+            this.AtaqueBase = Aleatorio.Next(1, 50);
+
+        }
+        public void GanharXp(int experiencia)
+        {
+            Xp += experiencia;
+            int novoNivel = (Xp / 10) + 1;
+            if (novoNivel > Nivel)
+            {
+                Console.WriteLine("\nVocê aumentou de nível!");
+                Vida = novoNivel * 10;
+            }
+            Nivel = novoNivel;
+            AtaqueBase = AtaqueBase + Nivel;
+        }
+
+
+    }
+}
--- jogoRpg/Jogo.cs
+++ jogoRpg/Jogo.cs
@@ -8,21 +8,25 @@ namespace jogoRpg
 {
     public class Jogo
     {
+        Heroi heroi;
         public void Iniciar()
         {
             Console.WriteLine("Digite seu nome:");
-            Console.ReadLine();
+             heroi = new Heroi(Console.ReadLine());
             Menu();
         }
         internal void Menu()
         {
             Console.Clear();
+            MostrarInfo();
             Console.WriteLine("Escolha qual monstro você deseja lutar");
             Console.WriteLine("1 - ORC ");
             Console.WriteLine("2 - TROLL");
             Console.WriteLine("3 - GLOBIN");
+            Console.WriteLine("");
+            Console.WriteLine("Digite 0 para sair do jogo");
 
-            switch(Console.ReadLine().ToLower())
+            switch (Console.ReadLine().ToLower())
             {
                 case "1":
                 case "orc":
@@ -45,7 +49,27 @@ namespace jogoRpg
             }
             Console.WriteLine("Pressione qualquer tecla para continuar...");
             Console.ReadKey();
+            if (heroi.Vida <= 0)
+            {
+                Console.WriteLine("Você faleceu...");
+                return;
+            }
+            else
+            {
+                Menu();
+            }
 
+        }
+            
+       void MostrarInfo()
+       {
+            Console.WriteLine($"Olá {heroi.Nome}");
+            Console.WriteLine($"Sua vida é: {heroi.Vida}");
+            Console.WriteLine($"Seu nível é: {heroi.Nivel}");
+            Console.WriteLine($"Seu ataque é: {heroi.Ataque}");
+            Console.WriteLine($"Sua experiência é: {heroi.Xp}");
+            Console.WriteLine($"Seu ataque base é: {heroi.AtaqueBase}");
+            Console.WriteLine("");
         }
     }
 }
